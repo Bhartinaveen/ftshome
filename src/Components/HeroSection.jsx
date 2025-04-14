719760bdcec224da9638359bf2e6fdbef31bb915
Naveen Bharti <bhartinaveen943@gmail.com>
2025-04-15 04:49:33 +0530

    chan

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,34 +1,18 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 
 const HeroSection = () => {
-  const imageref = useRef();
-  const [objectYOffset, setObjectYOffset] = useState(0);
-
-  useEffect(() => {
-    const updateYOffset = () => {
-      if (imageref.current) {
-        setObjectYOffset(imageref.current.offsetWidth);
-      }
-    };
-
-    updateYOffset();
-    window.addEventListener("resize", updateYOffset);
-
-    return () => window.removeEventListener("resize", updateYOffset);
-  }, []);
-
   return (
-    <div className="relative w-full overflow-hidden" >
+    <div className="relative w-full overflow-hidden">
       <img
-        ref={imageref}
-        className={`w-full h-[100px] sm:h-[100px] md:h-[400px] object-cover object-[center_${objectYOffset}px]` }
+        className="w-full h-[40vh] sm:h-[40vh] md:h-[50vh] lg:h-[70vh] object-cover object-center"
         src="/images/hl.avif"
         alt="Hero"
-       />
+      />
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white text-3xl font-bold text-center">
-          {/* <span className="text-blue-500">Discover Extraordinary</span>{" "}
-          <span className="text-orange-500">Comfort in Hotels</span> */}
+        <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold text-center px-4">
+          {/* Example Heading */}
+          <span className="text-blue-100">Discover Extraordinary</span>{" "}
+          <span className="text-orange-500">Comfort in Hotels</span>
         </h1>
       </div>
     </div>
